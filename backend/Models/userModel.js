ee123d30ff6ddfb2238d4a8cde486e14bcaaa61d
mostafa <mostafa.bebars37@gmail.com>
2025-12-06 23:52:43 +0200

    bug fix: export module properly and add user role handling

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -30,7 +30,7 @@ const userSchema = new Schema({
 })
 
 
-userSchema.statics.signup = async function(username, email, password) {
+userSchema.statics.signup = async function(username, email, password, role) {
 
 
     // validation
@@ -56,7 +56,7 @@ userSchema.statics.signup = async function(username, email, password) {
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
-    const user = await this.create({ username, email, password: hash })
+    const user = await this.create({ username, email, password: hash, role })
 
     return user
 }

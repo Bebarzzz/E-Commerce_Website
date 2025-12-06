a73f2385b7805f3f4bbe32b0f37d21377ecfa589
ziadz999 <ziadkhaled2424@gmail.com>
2025-12-07 00:07:56 +0200

    Added frontend into Admin-Panel-Front

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,37 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+
+let new_collections = [
+  {
+    id: 1,
+    name: "2024 Toyota Camry SE",
+    image: p1_img,
+    new_price: 28000.0,
+    old_price: 29500.0,
+  },
+  {
+    id: 2,
+    name: "2024 Honda Civic Sport",
+    image: p2_img,
+    new_price: 26000.0,
+    old_price: 27500.0,
+  },
+  {
+    id: 3,
+    name: "2024 Ford Mustang GT",
+    image: p3_img,
+    new_price: 45000.0,
+    old_price: 48000.0,
+  },
+  {
+    id: 4,
+    name: "2024 Chevrolet Silverado 1500",
+    image: p4_img,
+    new_price: 52000.0,
+    old_price: 55000.0,
+  },
+];
+
+export default new_collections;
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+export const Navbar = () => {
+
+  const [menu,setMenu] = useState("Inventory");
+  const { getTotalCartItems } = useContext(ShopContext);
+
+  return (
+    <div className='navbar'>
+      <div className ='nav-logo'>
+        <Link to='/' style={{ textDecoration: 'none', display: 'flex', alignItems: 'center', gap: '10px' }} onClick={() => setMenu("Inventory")}>
+          <img src={logo} alt='' />
+          <p>Automotive</p>
+        </Link>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("Inventory")}><Link style={{ textDecoration: 'none' }} to='/'>Inventory</Link>{menu === "Inventory" && <hr />}</li>
+        <li onClick={() => setMenu("New cars")}><Link style={{ textDecoration: 'none' }} to='/new-cars'>New cars</Link>{menu === "New cars" && <hr />}</li>
+        <li onClick={() => setMenu("Used cars")}><Link style={{ textDecoration: 'none' }} to='/used-cars'>Used cars</Link>{menu === "Used cars" && <hr />}</li>
+        <li onClick={() => setMenu("Offers")}><Link style={{ textDecoration: 'none' }} to='/offers'>Offers</Link>{menu === "Offers" && <hr />}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/login'><button>Login</button></Link>
+        <div className="nav-cart-container">
+          <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+          <div className="nav-cart-count">{getTotalCartItems()}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

361b03b4a3c5304fc7c3edbf1de66dd123f3e7ec
ziadz999 <ziadkhaled2424@gmail.com>
2025-12-12 18:11:29 +0200

    adjusted the cart items

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -15,24 +15,22 @@ const CartItems = () => {
         <p>Total</p>
         <p>Remove</p>
       </div>
-      <hr />
+      <div className="cartitems-list">
       {all_product.map((e)=>{
         if(cartItems[e.id]>0)
         {
-            return <div>
-                        <div className="cartitems-format cartitems-format-main">
-                            <img src={e.image} alt="" className='carticon-product-icon' />
-                            <p>{e.name}</p>
-                            <p>${e.new_price}</p>
-                            <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                            <p>${e.new_price*cartItems[e.id]}</p>
-                            <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
-                        </div>
-                        <hr />
+            return <div key={e.id} className="cartitems-format cartitems-format-main">
+                        <img src={e.image} alt="" className='carticon-product-icon' />
+                        <p>{e.name}</p>
+                        <p>${e.new_price}</p>
+                        <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                        <p>${e.new_price*cartItems[e.id]}</p>
+                        <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
                     </div>
         }
         return null;
       })}
+      </div>
       <div className="cartitems-down">
         <div className="cartitems-total">
             <h1>Cart Totals</h1>

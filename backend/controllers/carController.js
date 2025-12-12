9de04795b17d0b88057a1de5b333d79e2fbfd87a
ziadz999 <ziadkhaled2424@gmail.com>
2025-12-12 21:36:03 +0200

    Restore backend directory

---
+++ backend/controllers/carController.js
@@ -0,0 +1,35 @@
+const Car = require('../Models/carModel')
+
+
+const removeCar = async (req, res) => {
+    const { carId } = req.body;
+
+    try {
+        const result = await Car.removeCar(carId);
+
+        if (result.success) {
+            res.status(200).json({ message: result.message, deletedCar: result.deletedCar });
+        } else {
+            res.status(404).json({ error: result.message });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+const addCar = async (req, res) => {
+    const { model, manufactureYear, brand, type, price } = req.body;
+
+    try {
+        const car = await Car.addNewCar(model, manufactureYear, brand, type, price);
+
+        res.status(201).json(car);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = {
+    addCar,
+    removeCar,
+};
